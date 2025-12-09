8f369a0cc7500c496531103dc1db099996eb8bcf
Allen Zhu <allenzhu@Allendebijibendiannao.local>
2025-12-09 14:27:54 +0800

    new github build pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PhiloSphereDemo/',   // ⭐⭐ 关键：你的仓库名
+  base: '/PhiloSphereDemo/',   // ⭐⭐ 关键：你的仓库名123
   plugins: [react()],
 })
