c67ea62125dde4293822987b8c5b8b31e313b86b
allenzhu312 <allenzhu312@gmail.com>
2025-12-09 14:15:45 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PhiloSphereDemo/',   // ⭐⭐ 关键：GitHub Pages 子路径
+  base: '/PhiloSphereDemo/',   // ⭐⭐ 关键：你的仓库名
   plugins: [react()],
 })
